e0a24a26ac437b74b6a9f45cfca6204c73cb7331
yanggaoxiang <yanggaoxiang@cmss.chinamobile.com>
2026-05-29 14:25:59 +0800

    Modernize landing page

--- main.js
+++ main.js
@@ -1,169 +1,73 @@
-// (function($, window) {
-//     function Constellation(canvas, options) {
-//         var $canvas = $(canvas),
-//             context = canvas.getContext("2d"),
-//             defaults = {
-//                 star: {
-//                     color: "rgba(255, 255, 255, .5)",
-//                     width: 2,
-//                     randomWidth: true
-//                 },
-//                 line: {
-//                     color: "rgba(255, 255, 255, .5)",
-//                     width: 0.2
-//                 },
-//                 position: {
-//                     x: 0,
-//                     y: 0
-//                 },
-//                 width: window.innerWidth,
-//                 height: window.innerHeight,
-//                 velocity: 0.1,
-//                 length: 100,
-//                 distance: 120,
-//                 radius: 250,
-//                 stars: []
-//             },
-//             config = $.extend(true, {}, defaults, options);
-//
-//         function Star() {
-//             this.x = Math.random() * canvas.width;
-//             this.y = Math.random() * canvas.height;
-//
-//             this.vx = (config.velocity - (Math.random() * 0.5));
-//             this.vy = (config.velocity - (Math.random() * 0.5));
-//
-//             this.radius = config.star.randomWidth ? (Math.random() * config.star.width) : config.star.width;
-//         }
-//
-//         Star.prototype = {
-//             create: function() {
-//                 context.beginPath();
-//                 context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-//                 context.fill();
-//             },
-//
-//             animate: function() {
-//                 var i;
-//                 for (i = 0; i < config.length; i++) {
-//
-//                     var star = config.stars[i];
-//
-//                     if (star.y < 0 || star.y > canvas.height) {
-//                         star.vx = star.vx;
-//                         star.vy = -star.vy;
-//                     } else if (star.x < 0 || star.x > canvas.width) {
-//                         star.vx = -star.vx;
-//                         star.vy = star.vy;
-//                     }
-//
-//                     star.x += star.vx;
-//                     star.y += star.vy;
-//                 }
-//             },
-//
-//             line: function() {
-//                 var length = config.length,
-//                     iStar,
-//                     jStar,
-//                     i,
-//                     j;
-//
-//                 for (i = 0; i < length; i++) {
-//                     for (j = 0; j < length; j++) {
-//                         iStar = config.stars[i];
-//                         jStar = config.stars[j];
-//
-//                         if (
-//                             (iStar.x - jStar.x) < config.distance &&
-//                             (iStar.y - jStar.y) < config.distance &&
-//                             (iStar.x - jStar.x) > -config.distance &&
-//                             (iStar.y - jStar.y) > -config.distance
-//                         ) {
-//                             if (
-//                                 (iStar.x - config.position.x) < config.radius &&
-//                                 (iStar.y - config.position.y) < config.radius &&
-//                                 (iStar.x - config.position.x) > -config.radius &&
-//                                 (iStar.y - config.position.y) > -config.radius
-//                             ) {
-//                                 context.beginPath();
-//                                 context.moveTo(iStar.x, iStar.y);
-//                                 context.lineTo(jStar.x, jStar.y);
-//                                 context.stroke();
-//                                 context.closePath();
-//                             }
-//                         }
-//                     }
-//                 }
-//             }
-//         };
-//
-//         this.createStars = function() {
-//             var length = config.length,
-//                 star,
-//                 i;
-//
-//             context.clearRect(0, 0, canvas.width, canvas.height);
-//
-//             for (i = 0; i < length; i++) {
-//                 config.stars.push(new Star());
-//                 star = config.stars[i];
-//
-//                 star.create();
-//             }
-//
-//             star.line();
-//             star.animate();
-//         };
-//
-//         this.setCanvas = function() {
-//             canvas.width = config.width;
-//             canvas.height = config.height;
-//         };
-//
-//         this.setContext = function() {
-//             context.fillStyle = config.star.color;
-//             context.strokeStyle = config.line.color;
-//             context.lineWidth = config.line.width;
-//         };
-//
-//         this.setInitialPosition = function() {
-//             if (!options || !options.hasOwnProperty("position")) {
-//                 config.position = {
-//                     x: canvas.width * 0.5,
-//                     y: canvas.height * 0.5
-//                 };
-//             }
-//         };
-//
-//         this.loop = function(callback) {
-//             callback();
-//
-//             window.requestAnimationFrame(function() {
-//                 this.loop(callback);
-//             }.bind(this));
-//         };
-//
-//         this.bind = function() {
-//             $canvas.on("mousemove", function(e) {
-//                 config.position.x = e.pageX - $canvas.offset().left;
-//                 config.position.y = e.pageY - $canvas.offset().top;
-//             });
-//         };
-//
-//         this.init = function() {
-//             this.setCanvas();
-//             this.setContext();
-//             this.setInitialPosition();
-//             this.loop(this.createStars);
-//             this.bind();
-//         };
-//     }
-//
-//     $.fn.constellation = function(options) {
-//         return this.each(function() {
-//             var c = new Constellation(this, options);
-//             c.init();
-//         });
-//     };
-// })($, window);
+(function() {
+    var rainEngine = null;
+
+    function startRain(image) {
+        if (rainEngine || typeof RainyDay === "undefined") {
+            return;
+        }
+
+        var crop = getCoverCrop(image, window.innerWidth, window.innerHeight);
+
+        rainEngine = new RainyDay({
+            image: image,
+            parentElement: document.querySelector(".hero"),
+            blur: 0,
+            opacity: 0.88,
+            crop: crop,
+            width: window.innerWidth,
+            height: window.innerHeight,
+            fps: 30,
+            enableCollisions: true,
+            gravityAngle: Math.PI / 2.08,
+        });
+
+        rainEngine.rain([[1, 2, 7000]]);
+        rainEngine.rain([[3, 3, 0.86], [5, 5, 0.9], [6, 2, 1]], 80);
+    }
+
+    function getCoverCrop(image, targetWidth, targetHeight) {
+        var imageWidth = image.naturalWidth;
+        var imageHeight = image.naturalHeight;
+        var imageRatio = imageWidth / imageHeight;
+        var targetRatio = targetWidth / targetHeight;
+        var cropWidth = imageWidth;
+        var cropHeight = imageHeight;
+        var cropX = 0;
+        var cropY = 0;
+
+        if (imageRatio > targetRatio) {
+            cropWidth = imageHeight * targetRatio;
+            cropX = (imageWidth - cropWidth) / 2;
+        } else {
+            cropHeight = imageWidth / targetRatio;
+            cropY = (imageHeight - cropHeight) / 2;
+        }
+
+        return [cropX, cropY, cropWidth, cropHeight];
+    }
+
+    function initRain() {
+        var image = document.querySelector(".hero-background");
+
+        if (!image) {
+            return;
+        }
+
+        image.crossOrigin = "anonymous";
+
+        if (image.complete && image.naturalWidth > 0) {
+            startRain(image);
+            return;
+        }
+
+        image.addEventListener("load", function() {
+            startRain(image);
+        }, { once: true });
+    }
+
+    if (document.readyState === "loading") {
+        document.addEventListener("DOMContentLoaded", initRain);
+    } else {
+        initRain();
+    }
+})();
